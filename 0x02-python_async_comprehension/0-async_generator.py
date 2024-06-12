b8c5d19d6799fa282f85c97cd0ab0dc07a067af3
Adam chriki <adam.chkp@gmail.com>
2024-06-12 03:14:15 +0100

    fix bugs async python tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-"""Creates a generator"""
+""" Async Generator """
 import asyncio
 import random
 from typing import Generator
 
-async def async_generator () -> Generator [float, None, None]:
-    for _ in range(0, 10):
-        """Each time asynchronously waits 1 second,
-        then yield a random number between 0 and 10"""
-        asyncio.sleep(1)
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+        Generate numbers
+
+        Args:
+            void
+
+        Return:
+            float time random
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
